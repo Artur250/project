87a7eb2f39a8533d19df692d18ff0291efdc5243
Artur250 <gameartur012@gmail.com>
2023-05-25 17:43:21 +0600

    header  update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom' 
+import icon from '../../assets/Frame.svg'
 import "./Header.css"
 
 const Header = () => {
  
         return (
             <div>
-                
+                  <div className="background-logo"> 
+                <a href='/' className='logo'></a> 
+            </div> 
+            <div className='container'> 
+                <a className="logo_company"> 
+                    <img src={icon} className='icon'/> 
+                    <h2 className='hobby'>Hobby</h2> 
+                </a> 
+                <div className="nav"> 
+                    <Link to='/' className='Link'>Home</Link> 
+                    <Link to='/about' className='Link'>AboutUs</Link> 
+                    <Link to='/service' className='Link'>Service</Link> 
+                    <Link to='/contact' className='Link'>Contact</Link> 
+                    <Link to='/' className='login'>Login</Link> 
+                </div> 
+            </div> 
             </div>
         );
     
