8a34679de7bf7bf4e1c758a51969a7edfc130d61
Artur250 <gameartur012@gmail.com>
2023-05-25 22:35:26 +0600

    finish data

--- src/components/Data/Data.jsx
+++ src/components/Data/Data.jsx
@@ -9,8 +9,29 @@ const Data = () => {
             <div>
                 <div className='data'>
                     <img src={img1} alt="" />
+                    <p className='p'>
+                    The War Room
+                    <br />
+                    Sign up for our new newsletter covering defence
+                    <br />
+                    and international security
+
+                    </p>
                     <img src={img2} alt="" />
+                    <p className='p'> 
+                    The A to Z of economics
+                    <br />
+                    Economic terms explained to you in plain English
+
+                    </p>
                     <img src={img3} alt="" />
+                    <p className='p'>
+                    The Intelligence
+                    <br />
+                    Our daily podcast examines how Mexico’s drug
+                    <br />
+                     cartels are expanding
+                    </p>
                 </div>
             </div>
         )
