f9e1b28c50a60f4866d73ae69c0221fcd1bf76fa
Artur250 <gameartur012@gmail.com>
2023-05-25 22:45:36 +0600

    new folder briefing

---
+++ src/components/Briefing/Briefing.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Briefing =()=> {
+   
+        return (
+            <div>
+                
+            </div>
+        );
+    
+}
+
+export default Briefing;
