940c03c277ceda9c516b85025908d3592b7812ac
Artur250 <gameartur012@gmail.com>
2023-05-25 21:24:59 +0600

    new folder Data

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,14 +14,14 @@ const Header = () => {
                 
                 </a> 
                 <div className="nav"> 
-                    <Link to='/logo' className='Link'>The
+                    <Link to='/logo' className='Logo'>The
                                                     <br />
                                                 Economist
                     </Link> 
                     <Link to='/menu' className='Link'>Menu </Link> 
                     <Link to='/weekly' className='Link'>Weekly edition</Link> 
                     <Link to='/world' className='Link'>The world in brief</Link> 
-                    <Link to='/search' className='login'><input type="text" /></Link> 
+                    <Link to='/search' className='search'><input type="text" /></Link> 
                 </div> 
             </div> 
             </div>
